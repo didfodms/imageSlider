11f84d9373469a060a5484e733a0203ef85ff2b0
rae-eun <didfodms@gmail.com>
2022-06-18 22:47:55 +0900

    image slider

---
+++ src/js/imageSlider.js
@@ -0,0 +1,152 @@
+export default class ImageSlider {
+  #currentPosition = 0;
+
+  #slideNumber = 0;
+
+  #slideWidth = 0;
+
+  #intervalId;
+
+  #autoPlay = true;
+
+  sliderWrapEl;
+
+  sliderListEl;
+
+  nextBtnEl;
+
+  previousBtnEl;
+
+  indicatorWrapEl;
+
+  controlWrapEl;
+
+  // constructor 순서 유의
+  constructor() {
+    this.assignElement();
+    this.initSliderNumber();
+    this.initSliderWidth();
+    this.initSliderListWidth();
+    this.addEvent();
+    this.createIndicator();
+    this.setIndicator();
+    this.initAutoPlay();
+  }
+
+  assignElement() {
+    this.sliderWrapEl = document.getElementById('slider-wrap');
+    this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+    this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+    this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
+    this.controlWrapEl = this.sliderWrapEl.querySelector('#control-wrap');
+  }
+
+  initAutoPlay() {
+    this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+  }
+
+  initSliderNumber() {
+    this.#slideNumber = this.sliderListEl.querySelectorAll('li').length;
+  }
+
+  initSliderWidth() {
+    this.#slideWidth = this.sliderWrapEl.clientWidth;
+  }
+
+  initSliderListWidth() {
+    this.sliderListEl.style.width = `${this.#slideNumber * this.#slideWidth}px`;
+  }
+
+  addEvent() {
+    this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
+    this.controlWrapEl.addEventListener('click', this.togglePlay.bind(this));
+  }
+
+  togglePlay(event) {
+    if (event.target.dataset.status === 'play') {
+      this.#autoPlay = true;
+      this.controlWrapEl.classList.add('play');
+      this.controlWrapEl.classList.remove('pause');
+      this.initAutoPlay();
+    } else if (event.target.dataset.status === 'pause') {
+      this.#autoPlay = false;
+      this.controlWrapEl.classList.add('pause');
+      this.controlWrapEl.classList.remove('play');
+      clearInterval(this.#intervalId);
+    }
+  }
+
+  moveToLeft() {
+    this.#currentPosition -= 1;
+    if (this.#currentPosition === -1) {
+      this.#currentPosition = this.#slideNumber - 1;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+    if (this.#autoPlay === true) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
+    this.setIndicator();
+  }
+
+  moveToRight() {
+    this.#currentPosition += 1;
+    if (this.#currentPosition === this.#slideNumber) {
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+    if (this.#autoPlay === true) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
+    this.setIndicator();
+  }
+
+  createIndicator() {
+    // docFragment에 li들을 만들어서 ul태그 안에 넣는 작업
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#slideNumber; i += 1) {
+      const li = document.createElement('li');
+      // li태그의 data-index 속성값을 i로 설정
+      li.dataset.index = i;
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  setIndicator() {
+    // 옵셔널 체이닝 ?.
+    // index 전부 비활성화 -> 현재 슬라이드인 인덱스 활성화 하는 로직
+    // 활성화 된 것은 active 클래스로 표현 (classList를 활용한 클래스 add & remove)
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+    // nth-child는 ul 태그의 li가 몇번째 child이냐.. 이걸 나타냄
+    // nth-child는 1부터 시작하므로 this.#currentPosition + 1을 해주었음
+    this.indicatorWrapEl
+      .querySelector(`ul li:nth-child(${this.#currentPosition + 1})`)
+      .classList.add('active');
+  }
+
+  onClickIndicator(event) {
+    // parseInt 해주는 이유 : event.target.dataset.index가 string타입임
+    // 10은 10진수로 바뀌었다라는 뜻
+    const indexPosition = parseInt(event.target.dataset.index, 10);
+    // indexPosition이 parseInt가 잘 되었다면
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.left = `-${
+        this.#slideWidth * this.#currentPosition
+      }px`;
+      this.setIndicator();
+    }
+  }
+}
